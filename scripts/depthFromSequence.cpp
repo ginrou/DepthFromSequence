855c82bad3be5456f94e0e76fa9f8132a0460945
ginrou <ginrou799@gmail.com>
2014-04-28 20:58:17 +0900

    WIP
    - 対応点を描画した
    - 無駄に変換しないでステレオ法してみた

--- scripts/depthFromSequence.cpp
+++ scripts/depthFromSequence.cpp
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]) {
   // initialize trackers
   {
     vector<Point2f> points;
-    cv::goodFeaturesToTrack( input_images[0], points, MAX_CORNERS, 0.01, 10);
+    cv::goodFeaturesToTrack( input_images[0], points, MAX_CORNERS, 0.01, 10, noArray(), 5, true, 0.04);
     cv::cornerSubPix(input_images[0], points, sub_pix_win_size, cv::Size(-1,-1), term_crit );
     track_points.push_back(points);
   }
@@ -52,7 +52,7 @@ int main(int argc, char* argv[]) {
   }
 
   // construct fund matrix and rectify
-  Mat fund_mat, rectified[2];
+  Mat fund_mat, rectified[2], H1, H2;
   {
     std::vector<Point2f> points1, points2;
     for( int i = 0; i < total_status.size(); ++i ){
@@ -66,7 +66,6 @@ int main(int argc, char* argv[]) {
     std::vector<uchar> find_fund_mat_status;
     fund_mat = findFundamentalMat( points1, points2, find_fund_mat_status );
 
-    Mat H1, H2;
     stereoRectifyUncalibrated( points1, points2, fund_mat, img_size, H1, H2);
 
     warpPerspective( input_images.front(), rectified[0], H1, img_size, INTER_LINEAR, BORDER_REPLICATE);
@@ -75,10 +74,35 @@ int main(int argc, char* argv[]) {
     imwrite("tmp/img_00.png", rectified[0]);
     imwrite("tmp/img_01.png", rectified[1]);
 
-  }
+    // draw correspondences
+    {
+      Mat1b img( img_size.height, img_size.width*2);
+      cout << img.size() << endl;
+      input_images.front().copyTo( img(Rect(0,0,img_size.width, img_size.height)));
+      input_images.back().copyTo( img(Rect(img_size.width,0,img_size.width, img_size.height)));
 
+      cv::Scalar white( 255, 255, 255);
+
+      for( int i = 0; i < points1.size(); ++i ) {
+	Point2f pt1 = points1[i];
+	circle(img, pt1, 2.0, white, 1, 8, 0);
+
+	Point2f pt2 = points2[i];
+	pt2.x += img_size.width;
+	circle(img, pt2, 2.0, white, 1, 8, 0);
+
+	line(img, pt1, pt2, white);
+
+      }
+      
+      imwrite("tmp/matches.png", img);
+
+    }
+
+  }
+				   
   // calc disparity
-  Mat disp_map( rectified[0].size(), CV_32F );
+  Mat disp_map( img_size, CV_32F );
   {
     cv::StereoSGBM stereo;
     stereo.minDisparity = 0.0;
@@ -89,8 +113,11 @@ int main(int argc, char* argv[]) {
     stereo.P2 = 64 * 10 * 10;
     stereo.preFilterCap = 20.0;
     stereo.disp12MaxDiff = 0;
-    stereo( rectified[0], rectified[1], disp_map);
+    stereo( input_images[0], input_images[1], disp_map);
     disp_map /= 16.0;
+    Mat warped_disp_map;
+    warpPerspective( disp_map , warped_disp_map, H1.inv(), img_size, INTER_LINEAR, BORDER_REPLICATE);
+
   }
 
 
